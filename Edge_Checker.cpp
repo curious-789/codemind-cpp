c1ff143bf192799c38a32a5124f20b20eb648ad1
charan <84625492+curious-789@users.noreply.github.com>
2022-08-24 09:49:07 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int a,b;     cin>>a>>b;     if(b==a+1 || b==a+9 ||a==b+1 ||a==b+9)     cout<<"Yes";     else     cout<<"No";     return 0; }

---
+++ Edge_Checker.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    if(b==a+1 || b==a+9 ||a==b+1 ||a==b+9)
+    cout<<"Yes";
+    else
+    cout<<"No";
+    return 0;
+}
